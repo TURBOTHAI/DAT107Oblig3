94f8def8c9be53114bcfb0b60876331055550da5
798118 <164620581+TURBOTHAI@users.noreply.github.com>
2025-03-25 14:56:01 +0100

    sql update
    denne funker

--- bedrift.sql
+++ bedrift.sql
@@ -1,5 +1,5 @@
 -- Skript for å opprette databasen og legge inn litt data
-    -- Skjema = bedrift
+   -- Skjema = bedrift
         -- Tabell(er) = Ansatt, Prosjekt og avdeling 
 
 -- MERK!!! DROP SCHEMA ... CASCADE sletter alt !!!
@@ -10,22 +10,7 @@ SET search_path TO bedrift;
 -- Ikke nødvendig å slette tabellen(e) siden vi har tomt skjema, men ...
 -- DROP TABLE IF EXISTS person CASCADE;
 
-	private Integer avdelings_id;
-	private String navn;
-	private String ansatt_sjef;
-	
-CREATE TABLE Avdeling 
-	
-(
-	avdelings_id SERIAL PRIMARY KEY,
-	navn VARCHAR(100) NOT NULL,
-	ansatt_sjeff INT NOT NULL,
-	CONSTRAINT fk_ansatt_sjef FOREIGN KEY (ansatt_sjef) REFERENCES Ansatt(ansatt_id) ON DELTE RESTRICT
-	
-	
-);
 
-	
 CREATE TABLE Ansatt
 (
     ansatt_id SERIAL PRIMARY KEY,
@@ -33,25 +18,45 @@ CREATE TABLE Ansatt
     fornavn VARCHAR(255) NOT NULL,
     etternavn VARCHAR(255) NOT NULL,
     datoForAnsettelse DATE NOT NULL,
-    stilling VARCHAR(100) NOT NULL;
-    maanendsLonn INT NOT NULL;
-    avdelings_id INT NOT NULL;
-    CONSTRAINT fk_avdeling FOREIGN KEY (avdelings_id) REFERENCES Avdeling(Avdelings_id) ON DELETE RESTRICT
-    
+    stilling VARCHAR(100) NOT NULL,
+    maanedsLonn INT NOT NULL,
+    avdelings_id INT
+ -- Midlertidig uten NOT NULL og constraint
+ -- dette er på grunn av at vi kan møte på en sirkulær avhengighet viss vi lager en constraint som er avhenig av avdeling
 );
+	
+CREATE TABLE Avdeling 
+	
+(
+	avdelings_id SERIAL PRIMARY KEY,
+	navn VARCHAR(100) NOT NULL,
+	ansatt_sjef INT NOT NULL,
+	CONSTRAINT fk_ansatt_sjef FOREIGN KEY (ansatt_sjef) REFERENCES Ansatt(ansatt_id) ON DELETE RESTRICT
+	--avdeling blir avhengig av ansatt
+	
+);
+
+
+
+
+-- teoretisk sett så trenger vi ikkje å gjøre dette siden en avdeling trenger teknisk sett ikkje ansatte for at det skal være en avdeling, men her blir det gjort bare for selve kompetansen
+-- no legges det til en foreign key constraint i Ansatt som gjør ansatt avhengig i avdeling
+	
+
 
 
 CREATE TABLE Prosjekt
 (	
 	prosjekt_id SERIAL PRIMARY KEY,
-	navn; VARCHAR(100) NOT NULL,
+	navn VARCHAR(100) NOT NULL,
 	Beskrivelse TEXT NOT NULL
 	
 	
 );
 
 
-CREATE TABLE Ansatt_Prosjekt(
+CREATE TABLE Ansatt_Prosjekt
+(
 	ansatt_id INT NOT NULL,
 	prosjekt_id INT NOT NULL,
 	rolle VARCHAR(100) NOT NULL,
@@ -63,19 +68,36 @@ CREATE TABLE Ansatt_Prosjekt(
 );
 
 
+INSERT INTO ansatt (brukernavn, fornavn, etternavn, datoForAnsettelse,stilling, maanedsLonn, avdelings_id)
 
-INSERT INTO ansatt (brukernavn, fornavn, etternavn, datoForAnsettelse,stilling, maanendsLonn, avdelings_id)
+
 VALUES
-	('cstr',"christopher","strandheim",'2024-02-01','Utvikler',50000,1);
+	('cstr','christopher','strandheim','2024-02-01','Utvikler',50000,null),
+	('dkron','daniel','kronheim','2024-02-01','Utviklrt',60000,2);
+	
+INSERT INTO Avdeling (navn, ansatt_sjef)
+VALUES ('IT-avdelingen', 1);
+VALUES ('PT-avdelingen',2);
+
+
+
+
+	
+
+UPDATE Ansatt SET avdelings_id = 1 WHERE ansatt_id = 1;
+
+ALTER TABLE Ansatt ALTER COLUMN avdelings_id SET NOT NULL;
 
 INSERT INTO Prosjekt (navn, beskrivelse)
 VALUES
     ('Webutvikling', 'Utvikling av ny webapplikasjon for intern kommunikasjon.');
 
 
-INSERT INTO Ansatt_Prosjekt(ansatt_id, prosjekt_id, rolle, antall:jobbtimer)
+INSERT INTO Ansatt_Prosjekt(ansatt_id, prosjekt_id, rolle, antall_jobbtimer)
 VALUES
 	(1,1,'Prosjektleder',120);
 
-SELECT * FROM ansatt;
+
+
+SELECT * FROM Ansatt;
 
